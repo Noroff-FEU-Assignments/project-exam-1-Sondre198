8305acb48a1b500786eb6a80f83aed5112f5001c
Sondre Lie <120113424+Sondre198@users.noreply.github.com>
2023-10-11 22:57:59 +0200

    Update keys.js

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,2 @@
 export const keys="consumer_key=ck_459b2cdac335938cca9febcf70b16832176f7038&consumer_secret=cs_4054be385a362df493cccc68e4874b45bb952a98"
-export const api="https://cors.noroff.dev/taste-tales/wp-json/wc/store"
+export const api="https://comforting-tarsier-ff06de.netlify.app/wp-json/wc/store"
