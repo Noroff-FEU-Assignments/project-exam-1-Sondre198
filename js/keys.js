5c509a135dc336875b02925586f2815d6c7b2fad
Sondre Lie <120113424+Sondre198@users.noreply.github.com>
2023-10-16 23:52:14 +0200

    Update keys.js

--- js/keys.js
+++ js/keys.js
@@ -1,2 +1,2 @@
 export const keys="consumer_key=ck_459b2cdac335938cca9febcf70b16832176f7038&consumer_secret=cs_4054be385a362df493cccc68e4874b45bb952a98"
-export const api="https://tastetales.no/wp-json/wp/v2/posts?_embed"
+export const api="https://tastetales.no/wp-json/wp/v2"
